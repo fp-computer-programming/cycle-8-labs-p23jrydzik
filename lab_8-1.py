5e7970f466ad9354a77761d5cbbb3a29a72a6d45
Jack Rydzik <p23jrydzik@fairfieldprep.org>
2022-12-02 09:15:14 -0500

    Finished labs 1 and 2

---
+++ lab_8-1.py
@@ -0,0 +1,14 @@
+# author: JHR 12/2/22
+
+# defining empty variable
+money2 = 0
+
+# defining input range
+numero = int(input("What range do you want?"))
+
+# writing my loop!
+for money in range(numero):
+    money2 += money
+
+# printing sum 
+print(money2)
---
+++ lab_8-2.py
@@ -0,0 +1,17 @@
+# author: JHR 12/2/22
+
+# defining my list of names
+names = ["Bob", "Jim", "Bill"]
+
+# defining final print statement
+state = "Hi "
+
+# making my loop
+for name in names:
+    state = state + name + ", "
+
+# finishing my statement
+state = state + "you\'re invited to my party on Friday!"
+
+# printing final statement 
+print(state)
